aea6d997d430223f18c1f35083d132fcac2a9cc9
Linda Götenmark <ligo2200@student.miun.se>
2024-01-07 11:36:19 +0100

    app started with basic components, footer done, continuing on header

---
+++ client/src/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function () {
+  return (
+    <div>
+        <footer>
+            <p>&copy; Linda Götenmark, 2024</p>
+            <a href='mailto:ligo2200@student.miun.se'>ligo2200@student.miun.se</a>
+            <p>Projekt för kurs DT162G, Mittuniversitetet Sundsvall</p>
+
+        </footer>
+    </div>
+  )
+}
---
+++ client/src/StartPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Login from './Login';
+
+export default function StartPage() {
+  return (
+    <>
+    <div>
+        <h1>Välkommen till Catsite!</h1>
+    </div>
+
+    <Login />
+
+    </>
+    
+  )
+}
